ca73388600bf21eca7dba37d8c57bc960392c469
1inguini <9647142@gmail.com>
2025-07-04 13:47:06 +0900

    esbuildが悪いのか@cloudflare/workers-sharedが悪いのか

--- worker.js
+++ worker.js
@@ -1,13 +1,25 @@
-import { Hono } from "hono";
+// import { Hono } from "hono";
 
-const app = new Hono();
+// const app = new Hono();
 
-app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
+// app.get("/", (c) => c.text("Hello Cloudflare Workers!"));
+// app.get("/:client", (c) => {
+//   const { client } = c.req.param();
+//   c.header("X-Message", "Hi!");
+//   return c.text(`You want to see ${page} of ${id}`);
+// });
 
-// クラスをそのままexportできない
-// https://github.com/cloudflare/workerd/issues/699
+import * as app from "@cloudflare/workers-shared";
 export default {
   async fetch(req, env, ctx) {
     return app.fetch(req, env, ctx);
   },
 };
+
+// // クラスをそのままexportできない
+// // https://github.com/cloudflare/workerd/issues/699
+// export default {
+//   async fetch(req, env, ctx) {
+//     return app.fetch(req, env, ctx);
+//   },
+// };
