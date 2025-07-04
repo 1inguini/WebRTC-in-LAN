2e6c49bde603c6058caf736568d0c792c47f4f06
1inguini <9647142@gmail.com>
2025-07-04 16:48:22 +0900

    HonoがContent-Typeの面倒見てくれる

--- worker.js
+++ worker.js
@@ -1,33 +1,17 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { appendTrailingSlash } from "hono/trailing-slash";
 import { serveStatic } from "hono/serve-static";
-import mimeDB from "mime-db/db.json";
 
 const app = new Hono();
 
-const extDB = {};
-for (const [mime, v] of Object.entries(mimeDB)) {
-  for (const ext of v.extensions ?? []) {
-    extDB[ext] ??= {
-      contentType: v.charset
-        ? `${mime};charset=${v.charset.toLowerCase()}`
-        : mime,
-      ...v,
-    };
-  }
-}
-
+app.use(logger());
+app.use(appendTrailingSlash());
 app.use(
   serveStatic({
     getContent: async (path, ctx) => {
       const { body } = await ctx.env.files.fetch(ctx.req.raw);
-      const res = new Response(body, {
-        headers: {
-          "Content-Type":
-            extDB[path.split(".").pop()].contentType ??
-            "application/octet-stream",
-        },
-      });
-      return res;
+      return body;
     },
   }),
 );
