804bf16e89534c0bd8f5cb796c6a31a972d0791c
1inguini <9647142@gmail.com>
2025-07-07 15:33:07 +0900

    console.{ info => debug }

--- client/index.js
+++ client/index.js
@@ -9,25 +9,25 @@ const main = $("main");
 const streams = [{ audio: {} }, { video: {} }].map(async (constraint) => {
   try {
     const stream = m.getUserMedia(constraint);
-    console.info("Got MediaStream:", stream, "for", constraint);
+    console.debug("Got MediaStream:", stream, "for", constraint);
     return stream;
   } catch (e) {
-    console.info("Error accessing device", e, "for", constraint);
+    console.debug("Rejected accessing device", e, "for", constraint);
   }
 });
 
-console.info("MediaStreams:", streams);
+console.debug("MediaStreams:", streams);
 
 for (const promiseS of streams) {
   try {
     const s = await promiseS;
     const mediaStream = $("#template > .media-stream").cloneNode(true);
-    console.info("media-stream", mediaStream);
+    console.debug("media-stream", mediaStream);
     mediaStream.$(".media-stream.id").innerText = s.id;
     var hasVideo = false;
     const mediaTrack = $("#template > .media-track").cloneNode(true);
     for (const t of s.getTracks()) {
-      console.info("Got MediaStreamTrack:", t);
+      console.debug("Got MediaStreamTrack:", t);
       mediaTrack.$(".media-track.label").innerText = t.label;
       hasVideo ||= t.kind === "video";
       mediaStream.$("ul.media-tracks").appendChild(mediaTrack);
@@ -40,7 +40,7 @@ for (const promiseS of streams) {
     mediaStream.$("video, audio").srcObject = s;
     $("ul.media-streams").appendChild(mediaStream);
   } catch (e) {
-    console.info(e);
+    console.debug(e);
   }
 }
 
