76bb250344c43808bffc725ded72882655f0c085
1inguini <9647142@gmail.com>
2025-07-10 11:33:12 +0900

    trackのテンプレートが毎回ちゃんと複製されてなかった

--- client/index.js
+++ client/index.js
@@ -7,30 +7,15 @@ async function main() {
     document.importNode($(q.trimEnd() + ":is(template)").content, true);
 
   const m = navigator.mediaDevices;
-  const main = $("main");
 
-  // マイクとカメラを要求
-  const streams = (async function* () {
-    for (const constraint of [{ audio: {} }, { video: {} }]) {
-      try {
-        const stream = await m.getUserMedia(constraint);
-        console.info("Got MediaStream:", stream, "for", constraint);
-        yield stream;
-      } catch (e) {
-        console.info("Accessing device rejected ", e, "for", constraint);
-      }
-    }
-  })();
-  console.debug("MediaStreams:", streams);
-
-  // Webページ内に表示
-  for await (const s of streams) {
+  async function displayStream(stream) {
     const mediaStream = useTemplate(".media-stream");
     console.debug("media-stream", mediaStream);
-    mediaStream.$(".media-stream.id").innerText = s.id;
+    mediaStream.$(".media-stream.id").innerText = stream.id;
     let hasVideo = false;
-    const mediaTrack = useTemplate(".media-track");
-    for (const t of s.getTracks()) {
+    for (const t of stream.getTracks()) {
+      const mediaTrack = useTemplate(".media-track");
+      console.debug("media-track", mediaTrack);
       console.debug("Got MediaStreamTrack:", t);
       mediaTrack.$(".media-track.label").innerText = t.label;
       hasVideo ||= t.kind === "video";
@@ -41,16 +26,38 @@ async function main() {
     } else {
       mediaStream.$("video").remove();
     }
-    mediaStream.$("video, audio").srcObject = s;
+    mediaStream.$("video, audio").srcObject = stream;
     $("ul.media-streams").appendChild(mediaStream);
   }
 
+  // マイクとカメラを要求
+  const streams = async function* () {
+    for (const constraint of [{ audio: {} }, { video: {} }]) {
+      try {
+        const stream = await m.getUserMedia(constraint);
+        console.info("Got MediaStream:", stream, "for", constraint);
+        yield stream;
+      } catch (e) {
+        console.info("Accessing device rejected ", e, "for", constraint);
+      }
+    }
+  };
+  console.debug("MediaStreams:", streams);
+
+  // Webページ内に表示
+  for await (const s of streams()) {
+    displayStream(s);
+  }
+
+  // カメラとマイクからの入力を1つのストリームに
   const stream = new MediaStream();
-  for await (const s of streams) {
+  for await (const s of streams()) {
     for (const t of s.getTracks()) {
       stream.addTrack(t);
     }
   }
+  console.debug("Tracks:", stream.getTracks());
+  displayStream(stream);
 
   console.info("Aggregated MediaStream:", stream);
 
